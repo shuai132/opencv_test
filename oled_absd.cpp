b01e51e175c36d94dd1dfb480508750abd5d25c9
liushuai <770722922@qq.com>
2021-08-21 01:20:39 +0800

    feat: update

--- oled_absd.cpp
+++ oled_absd.cpp
@@ -1,12 +1,24 @@
-#include "stdio.h"
-#include "iostream"
+#include <memory>
 #include "opencv2/highgui.hpp"
 #include "opencv2/opencv.hpp"
 
 using namespace std;
 using namespace cv;
 
-#define MAX_NUMBER  INT_MAX
+#define IMG_WIDTH 128
+#define IMG_HEIGHT 64
+
+static void printHex(const uint8_t* data, size_t size, const std::string& name = "img") {
+    printf("%s: {", name.c_str());
+    for (size_t i = 0; i < size; ++i) {
+        printf("0x%X", data[i]);
+        if (i < size - 1) {
+            printf(", ");
+        } else {
+            printf("}\n");
+        }
+    }
+}
 
 int main() {
     VideoCapture cap;
@@ -19,40 +31,33 @@ int main() {
         return 0;
     }
 
-    system("mkdir out");
+    system("mkdir -p out");
 
     Mat img;
-    int frames = MAX_NUMBER;
-    while(frames--)
-    {
+    while(true) {
         cap.read(img);
         if (img.empty()) break;
-        static int frame_ct = 0;
-        static int picture_ct = frames+1;
-        int axis_x,axis_y;
         cvtColor(img, img, COLOR_RGB2GRAY);
         resize(img,img,Size(128,64),0,0,INTER_LINEAR);
-//            resize(img,img,Size(1280,640),0,0,INTER_LINEAR);
-//            threshold(img, img, 0, 255,  THRESH_TRIANGLE);
-//            adaptiveThreshold(img, img,255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY,251,10);
         adaptiveThreshold(img, img, 255, ADAPTIVE_THRESH_GAUSSIAN_C,THRESH_BINARY,25,10);
-//            resize(img,img,Size(1280,640),0,0,INTER_LINEAR);
-//            for(axis_x=0;axis_x<1280;axis_x++)
-//                for(axis_y=0;axis_y<640;axis_y++)
-//                    {
-//                        gray = (img.at<Vec3b>(Point(axis_x,axis_y))[0] + img.at<Vec3b>(Point(axis_x,axis_y))[1] + img.at<Vec3b>(Point(axis_x,axis_y))[2])/3;
-//                        gray = (gray>127?255:0);
-//                        img.at<Vec3b>(Point(axis_x,axis_y))[0] = gray;
-//					    img.at<Vec3b>(Point(axis_x,axis_y))[1] = gray;
-//					    img.at<Vec3b>(Point(axis_x,axis_y))[2] = gray;
-//                    }
+        auto binFrame = std::make_unique<uint8_t[]>(IMG_WIDTH / 8 * IMG_HEIGHT);
+        memset(binFrame.get(), 0, IMG_WIDTH / 8 * IMG_HEIGHT);
+        for (int h = 0; h < IMG_HEIGHT; h++) {
+            for (int w = 0; w < IMG_WIDTH; w++) {
+                const uint8_t& imgData = img.data[h * IMG_WIDTH + w];
+                uint8_t& binData = binFrame[h * IMG_WIDTH / 8 + w / 8];
+                if (imgData > 0) {
+                    binData |= 0b10000000 >> w % 8;
+                }
+            }
+        }
         imshow("gif",img);
-//            sprintf(gif,"%s%d%s","./out/",frame_ct,"_gif.bmp");
-//            resize(img,img,Size(128,64),0,0,INTER_LINEAR);
-//            Acquire_pot(frames,picture_ct,img);
-//            imwrite(gif,img);
+        char fileName[32];
+        static int frameCount;
+        sprintf(fileName, "%s%d%s", "./out/", frameCount++, ".bmp");
+        imwrite(fileName, img);
+        printHex(binFrame.get(), IMG_WIDTH / 8 * IMG_HEIGHT, fileName);
         waitKey(1000/cap.get(CAP_PROP_FPS));
-        frame_ct++;
     }
     cap.release();
     destroyWindow("gif");
