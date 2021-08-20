87d8a486405abdd6957f99f54c56e1b2a2a66748
liushuai <770722922@qq.com>
2021-08-20 11:21:34 +0800

    refine code

--- oled_absd.cpp
+++ oled_absd.cpp
@@ -1,19 +1,61 @@
-/*作者::XNY
-**引用::Opencv3.49
-**平台::x86_64
-**系统::Linux/Windows10
-**版本::2.5
-**时间::2020.2.12
-*/
-#include "oled_absd.h"
+#include "stdio.h"
+#include "iostream"
+#include "opencv2/highgui.hpp"
+#include "opencv2/opencv.hpp"
 
-int main(void)
-{
-    GRAY_DEAL();      /* 灰度处理视屏->二值化图像帧->取点 */
-//    Pot_buf_deal();   /* 阴码/阳码 */
-//    print_file();     /* 格式化输出 */
+using namespace std;
+using namespace cv;
 
-    cout<<"deal finsh!\n";
-    getchar();
+#define MAX_NUMBER  INT_MAX
+
+int main() {
+    VideoCapture cap;
+//    cap.open("1629390633805677.mp4");
+//    cap.open("1.gif");
+    cap.open("a.gif");
+//    cap.open(0);
+    if(!cap.isOpened()) {
+        cout<<"film open failed!\n";
+        return 0;
+    }
+
+    system("mkdir out");
+
+    Mat img;
+    int frames = MAX_NUMBER;
+    while(frames--)
+    {
+        cap.read(img);
+        if (img.empty()) break;
+        static int frame_ct = 0;
+        static int picture_ct = frames+1;
+        int axis_x,axis_y;
+        cvtColor(img, img, COLOR_RGB2GRAY);
+        resize(img,img,Size(128,64),0,0,INTER_LINEAR);
+//            resize(img,img,Size(1280,640),0,0,INTER_LINEAR);
+//            threshold(img, img, 0, 255,  THRESH_TRIANGLE);
+//            adaptiveThreshold(img, img,255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY,251,10);
+        adaptiveThreshold(img, img, 255, ADAPTIVE_THRESH_GAUSSIAN_C,THRESH_BINARY,25,10);
+//            resize(img,img,Size(1280,640),0,0,INTER_LINEAR);
+//            for(axis_x=0;axis_x<1280;axis_x++)
+//                for(axis_y=0;axis_y<640;axis_y++)
+//                    {
+//                        gray = (img.at<Vec3b>(Point(axis_x,axis_y))[0] + img.at<Vec3b>(Point(axis_x,axis_y))[1] + img.at<Vec3b>(Point(axis_x,axis_y))[2])/3;
+//                        gray = (gray>127?255:0);
+//                        img.at<Vec3b>(Point(axis_x,axis_y))[0] = gray;
+//					    img.at<Vec3b>(Point(axis_x,axis_y))[1] = gray;
+//					    img.at<Vec3b>(Point(axis_x,axis_y))[2] = gray;
+//                    }
+        imshow("gif",img);
+//            sprintf(gif,"%s%d%s","./out/",frame_ct,"_gif.bmp");
+//            resize(img,img,Size(128,64),0,0,INTER_LINEAR);
+//            Acquire_pot(frames,picture_ct,img);
+//            imwrite(gif,img);
+        waitKey(1000/cap.get(CAP_PROP_FPS));
+        frame_ct++;
+    }
+    cap.release();
+    destroyWindow("gif");
+    cout<<"finish!\n";
     return 0;
 }
